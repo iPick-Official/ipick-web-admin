9af6062c1dc04f7274c595e7e81095e981cf21b1
ipick <ipickdev@ipickride.com>
2025-08-30 18:41:04 +0800

    fix deploying issue

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,25 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
-
 export default function Dashboard() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const token = searchParams.get("token");
-
-  useEffect(() => {
-    // Ensure token is available and not already in localStorage
-    if (token) {
-      localStorage.setItem("authToken", token);
-      // Replace URL to remove the token query parameter
-      router.replace("/dashboard");
-    } else {
-      // Optionally handle the case where token is not present
-      // (e.g., show a message or redirect elsewhere)
-      console.log("No token found in the URL.");
-    }
-  }, [token, router]);
 
   return (
     <div className="p-8">
